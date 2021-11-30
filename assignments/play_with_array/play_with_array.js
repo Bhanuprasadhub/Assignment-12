635e329bc58eb8fa9446e66665db320985ab1e88
G Bhanu Prasad <gubbalausha11@gmail.com>
2021-11-30 14:46:29 +0530

    This is 12th Assign

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,6 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
+    var i;
+    var a = []
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            a.push(arr[i])
+        }
+    } return a
 
 }
 
@@ -18,6 +25,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    var i;
+    var aa = []
+    for (i = 0; i < arr.length; i++) {
+        c = arr[i] * n
+        aa.push(c)
+    } return aa
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +41,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    arr.splice(n, 1)
+    return arr
 }
 
 module.exports = {
